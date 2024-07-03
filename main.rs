99ec5ac155f81a5aaac899fd1b5a8ac36c24dfc9
zzucch <nikitamilovatskiy@gmail.com>
2024-07-03 12:11:51 +0300

    refactor: decrease nesting

--- main.rs
+++ main.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
-use reqwest::Error;
 use config::{Config, File};
+use reqwest::Error;
+use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct StatusResponse {
@@ -24,15 +24,55 @@ async fn get_server_status(ip: &str) -> Result<StatusResponse, Error> {
     Ok(response)
 }
 
-async fn send_telegram_message(
-    bot_token: &str,
-    chat_id: i64,
-    text: &str)
--> Result<(), Error> {
+async fn send_telegram_message(bot_token: &str, chat_id: i64, text: &str) -> Result<(), Error> {
     let url = format!("https://api.telegram.org/bot{}/sendMessage", bot_token);
     let params = [("chat_id", chat_id.to_string()), ("text", text.to_string())];
-    reqwest::Client::new().post(&url).form(&params).send().await?;
+    reqwest::Client::new()
+        .post(&url)
+        .form(&params)
+        .send()
+        .await?;
     Ok(())
+}
+
+async fn handle_server_status(
+    status: StatusResponse,
+    last_result: &mut i32,
+    telegram_config: &TelegramConfig,
+) {
+    let players_online = status.players.online;
+    let player_names: Vec<String> = status
+        .players
+        .sample
+        .unwrap_or_default()
+        .into_iter()
+        .map(|player| player.name)
+        .collect();
+
+    println!("Players online: {}", players_online);
+    if player_names.len() > 0 {
+        println!("Player names: {:?}", player_names);
+    }
+
+    if players_online == *last_result {
+        return;
+    }
+
+    if *last_result != -1 && players_online > 0 {
+        let message = format!("{}", players_online);
+
+        if let Err(e) = send_telegram_message(
+            &telegram_config.bot_token,
+            telegram_config.chat_id,
+            &message,
+        )
+        .await
+        {
+            eprintln!("Failed to send message: {}", e);
+        }
+    }
+
+    *last_result = players_online;
 }
 
 fn main() {
@@ -40,51 +80,31 @@ fn main() {
 
     settings.merge(File::with_name("config")).unwrap();
 
-    let telegram_config = settings.get::<TelegramConfig>("telegram").unwrap();
     let server_config = settings.get::<ServerConfig>("server").unwrap();
+    let telegram_config = settings.get::<TelegramConfig>("telegram").unwrap();
 
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap()
-        .block_on(async {
-            let mut last_result = -1;
+        .block_on(monitor_server_status(&server_config, &telegram_config));
+}
 
-            loop {
-                match get_server_status(&server_config.ip).await {
-                    Ok(status) => {
-                        let players_online = status.players.online;
-                        let player_names: Vec<String> = status.players.sample
-                            .unwrap_or_default()
-                            .into_iter()
-                            .map(|player| player.name)
-                            .collect();
+async fn monitor_server_status(server_config: &ServerConfig, telegram_config: &TelegramConfig) {
+    let mut last_result = -1;
 
-                        println!("Players online: {}", players_online);
-                        if player_names.len() > 0 {
-                            println!("Player names: {:?}", player_names);
-                        }
-
-                        if players_online != last_result {
-                            if last_result != -1 && players_online > 0 {
-                                let message = format!("{}", players_online);
-                                if let Err(e) = send_telegram_message(
-                                     &telegram_config.bot_token,
-                                     telegram_config.chat_id,
-                                     &message).await {
-                                    eprintln!("Failed to send message: {}", e);
-                                }
-                            }
-                            last_result = players_online;
-                        }
-                    },
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                    }
-                }
-                tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+    loop {
+        match get_server_status(&server_config.ip).await {
+            Ok(status) => {
+                handle_server_status(status, &mut last_result, &telegram_config).await;
             }
-        });
+            Err(e) => {
+                eprintln!("{}", e);
+            }
+        }
+
+        tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+    }
 }
 
 #[derive(Deserialize)]
